81282a98a8cca479b26af5149249d83d55ba00d8
tcsenpai <thecookingsenpai@countermail.com>
2024-05-01 01:45:13 +0200

    updated exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+// Common types and constants
 export * as types from './types'
+// Basic cryptographic and data manipulation functions
+export * as encryption from './encryption'
+export * as utils from './utils'
+//  Specific features of the SDK
 export * as xmlocalsdk from './multichain/localsdk'
 export * as xmwebsdk from './multichain/websdk'
