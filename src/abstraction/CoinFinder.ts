31ce52ce3e8516bd4c9faaebed3b14cb6b757184
tcsenpai <dev@tcsenpai.com>
2024-12-09 13:57:06 +0100

    skeleton methods for the finders

--- src/abstraction/CoinFinder.ts
+++ src/abstraction/CoinFinder.ts
@@ -1,35 +1,38 @@
 // Import the singleton instance of the Providers class (already registered with all providers)
-import Providers  from "./providers"
+import Providers from "./providers"
+import { tokenAddresses } from "./providers/CoinAddresses"
 
 // ! Use the Providers singleton with the methods of each chain RPC to find tokens
-/** 
- * ? For each call, we should probably select a random RPC 
+/**
+ * ? For each call, we should probably select a random RPC
  * ? from the Providers singleton and use that one in a for loop
  * ? that breaks when the call is successful.
  * ? This is to prevent any single RPC from being overloaded and
  * ? to be able to fail over to the next one in the list.
-*/
+ */
 
 export default class CoinFinder {
-    constructor() {
-
-    }
+    constructor() {}
 
     // TODO Implement supported chains (e.g. solana, multiversx, etc.) tokens cross search
 
     static async findSol(targetChain: string) {
-        // TODO
+        // TODO Validate the target chain id
+        // TODO Get the wrapped sol address for the target chain
     }
 
     static async findMultiversx(targetChain: string) {
-        // TODO
+        // TODO Validate the target chain id
+        // TODO Get the wrapped multiversx address for the target chain
     }
 
     static async findXRP(targetChain: string) {
-        // TODO
+        // TODO Validate the target chain id
+        // TODO Get the wrapped xrp address for the target chain
     }
 
     static async findBTC(targetChain: string) {
-        // TODO
+        // TODO Validate the target chain id
+        // TODO Get the wrapped btc address for the target chain
     }
 }
--- src/abstraction/EvmCoinFinder.ts
+++ src/abstraction/EvmCoinFinder.ts
@@ -1,32 +1,47 @@
 // Import the singleton instance of the Providers class (already registered with all providers)
 import Providers from "./providers"
-
+import { tokenAddresses } from "./providers/CoinAddresses"
 // ! Use the Providers singleton with the methods of each chain RPC to find tokens
-/** 
- * ? For each call, we should probably select a random RPC 
+/**
+ * ? For each call, we should probably select a random RPC
  * ? from the Providers singleton and use that one in a for loop
  * ? that breaks when the call is successful.
  * ? This is to prevent any single RPC from being overloaded and
  * ? to be able to fail over to the next one in the list.
-*/
+ */
 
 export default class EvmCoinFinder {
-    constructor() {
-
-    }
+    constructor() {}
 
     // TODO Method to find native and wrapped eth across evms (from mainnet to other evms)
     static async findNativeEth(targetChainIds: number[]) {
         // TODO
+        // TODO Get the wrapped eth address for the target chain
     }
 
     // TODO Method to find native and wrapped assets across evms (e.g. opt, arb, etc.)
-    static async findNativeAssets(sourceChainId: number, targetChainIds: number[]) {
+    static async findNativeAssets(
+        sourceChainId: number,
+        targetChainIds: number[],
+    ) {
         // TODO
     }
 
     // TODO Method to find token pairs on evms (from an evm to other(s) evm(s))
-    static async findTokenPairs(tokenAddress: string, sourceChainId: number, targetChainIds: number[]) {
+    static async findTokenPairs(
+        tokenAddress: string,
+        sourceChainId: number,
+        targetChainIds: number[],
+    ) {
         // TODO
+        // TODO Validate the base token address for the source chain
+        // TODO Get the wrapped token address for the target chain if known or return false
+    }
+
+    static getNativeforSupportedChain(chain: string, targetChainId: number) {
+        // ! Typize this for supported chains
+        // TODO validate the chain
+        // TODO get the target chain id
+        // TODO return the native address for the target chain
     }
 }
