f67dfcb5fc8f7660b4746ce76fff4bc809f61d64
tcsenpai <dev@tcsenpai.com>
2025-01-15 09:39:49 +0100

    added send payload for natives

--- src/types/native/index.ts
+++ src/types/native/index.ts
@@ -1,9 +1,1 @@
-// ! Add the native operation types here (start with send)
-
-export interface INativePayload {
-    nativeOperation: string
-    args: any[]
-    kwargs: { [key: string]: any }
-    resolved: boolean
-    result: boolean
-}
+export * from "./INativePayload"
