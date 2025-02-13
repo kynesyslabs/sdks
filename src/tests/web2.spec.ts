e7f3bd9c6e02270bb60353312b8c58f2094da70a
cwilvx <geoffreymungai45@gmail.com>
2025-02-13 16:14:01 +0300

    update web2calls
    + add web2test

---
+++ src/tests/web2.spec.ts
@@ -0,0 +1,28 @@
+import { EnumWeb2Methods } from "@/types"
+import { Demos } from "@/websdk"
+import { DemosWebAuth, prepareXMPayload } from "@/websdk"
+
+describe("Web2", () => {
+    test("Simple Web2 Transaction", async () => {
+        const identity = DemosWebAuth.getInstance()
+        await identity.create()
+
+        const rpc = "http://localhost:53550"
+        const demos = new Demos()
+        await demos.connect(rpc)
+        await demos.connectWallet(identity.keypair.privateKey as any)
+
+        const proxy = await demos.web2.createDahr(demos)
+        const res = await proxy.startProxy({
+            method: EnumWeb2Methods.GET,
+            url: "https://node2.demos.sh/info",
+        })
+
+        console.log(res)
+
+        const res2 = await proxy.stopProxy()
+        console.log(res2)
+
+        demos.disconnect()
+    })
+})
