a7944130faf8ad480b99297b47a209fdf9d4e1af
tcsenpai <dev@tcsenpai.com>
2025-03-06 13:38:31 +0100

    forgot as usual to export stuff

--- src/abstraction/index.ts
+++ src/abstraction/index.ts
@@ -15,6 +15,8 @@ import {
     Web2CoreTargetIdentityPayload,
     InferFromGithubPayload,
     GithubProof,
+    InferFromTwitterPayload,
+    TwitterProof,
 } from "@/types/abstraction"
 
 export {
@@ -27,4 +29,6 @@ export {
     Web2CoreTargetIdentityPayload,
     InferFromGithubPayload,
     GithubProof,
+    InferFromTwitterPayload,
+    TwitterProof,
 }
