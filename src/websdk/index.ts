554df983b25d974431d78e80c47cb064e061cbb3
SergeyG-Solicy <sergey.ghazaryan@solicy.net>
2025-04-14 16:28:45 +0400

    Exported bridge for websdk

--- src/websdk/index.ts
+++ src/websdk/index.ts
@@ -1,7 +1,7 @@
 // @ts-nocheck
-export { demos } from './demos'
-export { Demos } from './demosclass'
-export { DemosTransactions } from './DemosTransactions'
+export { demos } from "./demos"
+export { Demos } from "./demosclass"
+export { DemosTransactions } from "./DemosTransactions"
 
 export { RSA } from "./rsa"
 export { DemosWebAuth } from "./DemosWebAuth"
@@ -19,3 +19,6 @@ export { bufferize } from "./utils/bufferizer"
 export { required, _required } from "./utils/required"
 export { forgeToString, stringToForge } from "./utils/forge_converter"
 export * as skeletons from "./utils/skeletons"
+
+// Bridge
+export { RubicBridge } from "./bridge"
