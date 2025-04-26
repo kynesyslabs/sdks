de554d4152c79fe273267e1dfe4c0da47191af95
tcsenpai <dev@tcsenpai.com>
2025-04-26 10:19:25 +0200

    corrected imports

--- src/encryption/index.ts
+++ src/encryption/index.ts
@@ -1,7 +1,19 @@
-export * as FHE from './FHE'
-export * as PQC from './PQC/enigma'
-export * as zK from './zK'
+import {
+    encryptedObject,
+    SerializedEncryptedObject,
+    signedObject,
+    SerializedSignedObject,
+} from "./unifiedCrypto"
+export * as FHE from "./FHE"
+export * as PQC from "./PQC/enigma"
+export * as zK from "./zK"
 
-export { Cryptography } from './Cryptography'
-export { Hashing } from './Hashing'
-export { unifiedCrypto as ucrypto } from './unifiedCrypto'
+export { Cryptography } from "./Cryptography"
+export { Hashing } from "./Hashing"
+export { unifiedCrypto as ucrypto } from "./unifiedCrypto"
+export {
+    encryptedObject,
+    SerializedEncryptedObject,
+    signedObject,
+    SerializedSignedObject,
+} from "./unifiedCrypto"
