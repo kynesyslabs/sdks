990193e43b2e170d521b7526260a6613cf5f6cd2
cwilvx <geoffreymungai45@gmail.com>
2025-05-09 16:44:28 +0300

    release v2.2.33 (export rubic stuff from sdk)

--- src/bridge/index.ts
+++ src/bridge/index.ts
@@ -1,11 +1,17 @@
-import { BridgeTradePayload } from "@/types/bridge/bridgeTradePayload"
-import {
+export { BridgeTradePayload } from "@/types/bridge/bridgeTradePayload"
+export {
     ChainProviders,
     SupportedChains,
     SupportedTokens,
 } from "@/types/bridge/constants"
-
-export { BridgeTradePayload, ChainProviders, SupportedChains, SupportedTokens }
+import RubicBridge from "./rubicBridge"
+export {
+    BLOCKCHAIN_NAME,
+    CROSS_CHAIN_TRADE_TYPE,
+    CrossChainTrade,
+    RubicSdkError,
+    WrappedCrossChainTrade,
+} from "rubic-sdk"
 
 import { methods as NativeBridgeMethods } from "./nativeBridge"
 export { NativeBridgeMethods }
@@ -23,3 +29,4 @@ export {
     usdcContracts as NativeBridgeUSDCContracts,
     usdcAbi as NativeBridgeUSDCAbi,
 } from "./nativeBridgeTypes"
+export { RubicBridge }
